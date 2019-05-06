890a7c1750f69149061955fddb7c48fe8274d7a8
Chris Anderson <chand2048@hotmail.com>
2019-05-05 20:54:19 -0700

    Use chrome to deal with security and scrape entire shutterstock catalog, dump to file and generate a flat table.

---
+++ PullCatalogFromShutterstock/MergeToFlatFile.cs
@@ -0,0 +1,153 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace PullCatalogFromShutterstock
+{
+    class MergeToFlatFile
+    {
+        private List<Photo> photos = new List<Photo>();
+
+        public void Add(string filename)
+        {
+            Photo p = new Photo();
+            p.ExtractFromFile(filename);
+            this.photos.Add(p);
+        }
+
+        public void Save(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+
+            using (TextWriter writer = File.CreateText(filename))
+            {
+                foreach(Photo p in this.photos)
+                {
+                    p.Write(writer);
+                }
+
+                writer.Close();
+            }
+        }
+
+        private class Photo
+        {
+            public string id;
+            public string status;
+            public string category1;
+            public string category2;
+            public string description;
+            public bool isEditorial;
+            public string keywords;
+            public string filename;
+            public string uploadDate;
+            public string thumbnailURL;
+            public string thumbnailURL480;
+
+            public void ExtractFromFile(string filename)
+            {
+                using (TextReader r = File.OpenText(filename))
+                {
+                    string data = r.ReadToEnd();
+                    this.ExtractFromJson(data);
+                }
+            }
+
+            public void Write(TextWriter writer)
+            {
+                char delim = '\t';
+                writer.Write(this.id); writer.Write(delim);
+                writer.Write(this.status); writer.Write(delim);
+                writer.Write(this.category1); writer.Write(delim);
+                writer.Write(this.category2); writer.Write(delim);
+                writer.Write(this.description); writer.Write(delim);
+                writer.Write(this.isEditorial); writer.Write(delim);
+                writer.Write(this.keywords); writer.Write(delim);
+                writer.Write(this.filename); writer.Write(delim);
+                writer.Write(this.uploadDate); writer.Write(delim);
+                writer.Write(this.thumbnailURL); writer.Write(delim);
+                writer.Write(this.thumbnailURL480); writer.Write(delim);
+                writer.Write("\r\n");
+
+                writer.Flush();
+            }
+
+            private void parseID(JObject root)
+            {
+                this.id = this.getText(root, "data.id");
+                if (this.id.Length > 0 && this.id[0] == 'P')
+                {
+                    this.id = this.id.Substring(1);
+                }
+            }
+
+            private void parseKeywords(JObject root)
+            {
+                JToken j = root.SelectToken("data.keywords");
+                StringBuilder builder = new StringBuilder();
+                foreach (JToken kw in j.Children())
+                {
+                    if (builder.Length > 0)
+                    {
+                        builder.Append("~");
+                    }
+
+                    builder.Append(kw.ToString());
+                }
+
+                this.keywords = builder.ToString();
+            }
+
+            private bool ExtractFromJson(string data)
+            {
+                JObject root = JsonConvert.DeserializeObject(data) as JObject;
+
+                this.parseID(root);
+                this.parseKeywords(root);
+
+                this.status = this.getText(root, "data.status");
+                this.category1 = this.getText(root, "data.categories[0]");
+                this.category2 = this.getText(root, "data.categories[1]");
+                this.description = this.getText(root, "data.description");
+                this.isEditorial = this.getBool(root, "data.is_editorial");
+                this.filename = this.getText(root, "data.original_filename");
+                this.uploadDate = this.getText(root, "data.uploaded_date");
+                this.thumbnailURL = "https:" + this.getText(root, "data.thumbnail_url");
+                this.thumbnailURL480 = this.getText(root, "data.thumbnail_url_480");
+
+                return true;
+            }
+
+            private string getText(JToken root, string path)
+            {
+                JToken x = root.SelectToken(path);
+                if (x != null)
+                {
+                    return x.ToString();
+                }
+
+                return string.Empty;
+            }
+
+            private bool getBool(JToken root, string path)
+            {
+                string x = this.getText(root, path);
+                bool t;
+                if (bool.TryParse(x, out t))
+                {
+                    return t;
+                }
+
+                return false;
+            }
+        }
+    }
+}
--- PullCatalogFromShutterstock/Program.cs
+++ PullCatalogFromShutterstock/Program.cs
@@ -1,17 +1,121 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace PullCatalogFromShutterstock
 {
     class Program
     {
+        static private void SaveJsonFromSSID(IWebDriver webDriver, string path, string SSID)
+        {
+            string url = "https://submit.shutterstock.com/api/content_editor/media/P" + SSID;
+            string prefix = "{\"data\":{";
+            string suffix = "</pre></body><span";
+            string data = GetInnerPageSource(webDriver, prefix, suffix, url);
+            ForceWrite(path + SSID + ".json", data);
+        }
+
+        private static void ForceWrite(string fname, string data)
+        {
+            if (File.Exists(fname))
+            {
+                File.Delete(fname);
+            }
+
+            if (data != null)
+            {
+                using (TextWriter f = File.CreateText(fname))
+                {
+                    f.Write(data);
+                }
+            }
+        }
+
+        private static string GetInnerPageSource(IWebDriver webDriver, string prefix, string suffix, string url)
+        {
+            webDriver.Navigate().GoToUrl(url);
+            string pageSource = webDriver.PageSource;
+            
+            int start = pageSource.IndexOf(prefix);
+            int end = pageSource.IndexOf(suffix);
+
+            if (start > 0 && end > 0)
+            {
+                return pageSource.Substring(start, end - start);
+            }
+            else
+            {
+                return pageSource;
+            }
+        }
+
+        private static void EnumerateCatalog(IWebDriver webDriver, string path)
+        {
+            string prefix = "{\"responseHeader\":{";
+            string suffix = "</pre></body><span";
+            int page = 1;
+            int perPage = 100;
+            string catalogFormat = "https://submit.shutterstock.com/api/catalog_manager/media_types/all/items?filter_type=keywords&filter_value=&page_number={0}&per_page={1}&sort=popular";
+
+            int foundCount;
+            do
+            {
+                string url = string.Format(catalogFormat, page, perPage);
+                string data = GetInnerPageSource(webDriver, prefix, suffix, url);
+
+                JObject o = JsonConvert.DeserializeObject(data) as JObject;
+                JToken t = null;
+                foundCount = 0;
+
+                if (o.TryGetValue("data", out t))
+                {
+                    foreach (JToken photo in t.Children())
+                    {
+                        JToken SSID = photo.SelectToken("media_id");
+                        if (SSID != null)
+                        {
+                            SaveJsonFromSSID(webDriver, path, SSID.ToString());
+                            foundCount++;
+                        }
+                    }
+                }
+
+                page++;
+            } while (foundCount > 0);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            if (false)
+            {
+                ChromeOptions o = new ChromeOptions();
+                o.AddArgument("user-data-dir=C:\\Users\\chand\\AppData\\Local\\Google\\Chrome\\User Data");
+                o.AddArgument("profile-dir=C:\\Users\\chand\\AppData\\Local\\Google\\Chrome\\User Data");
+
+                IWebDriver webDriver;
+                webDriver = new ChromeDriver(o);
+
+                string path = "C:\\Photos\\Shutterstock\\";
+                EnumerateCatalog(webDriver, path);
+
+                webDriver.Close();
+                webDriver.Quit();
+            }
+            else
+            {
+                string folder = "C:\\Photos\\Shutterstock\\";
+                MergeToFlatFile merge = new MergeToFlatFile();
+                foreach(string filename in Directory.EnumerateFiles(folder, "*.json"))
+                {
+                    Console.WriteLine(filename);
+                    merge.Add(filename);
+                }
+
+                merge.Save(folder + "catalog.tsv");
+            }
         }
     }
 }
